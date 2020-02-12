cb75a8260ff6592387e580612b863053bfb86a79
Filipe Liu <fliu@anl.gov>
2020-02-12 01:25:44 -0600

    deps

--- scripts/list_maps.py
+++ scripts/list_maps.py
@@ -3,5 +3,7 @@ import modelseed_escher
 
 escher_seed = modelseed_escher.EscherManager(escher)
 
+print(escher_seed.escher.get_cache_dir())
+
 for m in escher_seed.list_maps('ModelSEED'):
     print(m)
